efa2b05eaef47fc08ff4b48ebe713cb0f5509b09
Zhori <aldur92@hotmail.com>
2015-02-04 16:44:25 +0100

    Started making skeletons

---
+++ src/green_elevator/controller/Controller.java
@@ -0,0 +1,15 @@
+package green_elevator.controller;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public Controller(){
+		
+	}
+
+}
--- src/green_elevator/controller/IndataReciever.java
+++ src/green_elevator/controller/IndataReciever.java
@@ -1,5 +1,20 @@
 package green_elevator.controller;
 
-public class IndataReciever {
+public class IndataReciever implements Runnable {
+
+	
+	public IndataReciever() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Read incoming messages and put them into a buffer
+	 * 
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
