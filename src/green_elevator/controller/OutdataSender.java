5566c603533d7fe42ccec7fb14abaafc8d8b1712
Zhori <aldur92@hotmail.com>
2015-02-18 13:53:43 +0100

    Implemented OutdataSender

--- src/green_elevator/controller/OutdataSender.java
+++ src/green_elevator/controller/OutdataSender.java
@@ -1,5 +1,42 @@
 package green_elevator.controller;
 
-public class OutdataSender {
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Optional;
+
+public class OutdataSender implements Runnable {
+
+	private MessageBuffer buffer;
+	private PrintWriter writer;
+	private MesssageTranslator translator;
+
+	public OutdataSender(OutputStream output, MessageBuffer buffer, MesssageTranslator translator) {
+
+		this.buffer = buffer;
+		this.writer = new PrintWriter(output, true);
+
+	}
+
+	/**
+	 * Start sending messages from the message buffer to the output stream, one at a time.
+	 * 
+	 */
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				// getMessage is a blocking call so the thread will wait for the buffer be none empty.
+				Optional<String> message = translator.getMessageString(buffer.getMessage());
+				if (message.isPresent()) {
+					writer.print(message.get());
+				}
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
 
 }
