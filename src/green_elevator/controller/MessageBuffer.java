32446210caa49f616769eeb7042df83ba01a9407
ragnhild karlsson <ragnhild.karlsson@gmail.com>
2015-02-18 12:40:48 +0100

    messageBuffer

--- src/green_elevator/controller/MessageBuffer.java
+++ src/green_elevator/controller/MessageBuffer.java
@@ -1,5 +1,38 @@
 package green_elevator.controller;
 
+import green_elevator.controller.message.Message;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class MessageBuffer {
 
+    private BlockingQueue<Message> buffer;
+
+    public MessageBuffer() {
+	this.buffer = new LinkedBlockingQueue<Message>();
+    }
+
+    /**
+     * Puts the message into the buffer, if no space is available until it is
+     * space available in the buffer
+     * 
+     * @param message
+     * @throws InterruptedException
+     */
+    public void putMessage(Message message) throws InterruptedException {
+	buffer.put(message);
+    }
+
+    /**
+     * Return the first element in the queue. Blocks until a message is
+     * available.
+     * 
+     * @param message
+     * @return
+     * @throws InterruptedException
+     */
+    public Message getMessage(Message message) throws InterruptedException {
+	return buffer.take();
+    }
 }
