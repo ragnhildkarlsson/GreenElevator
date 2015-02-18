4ffe74747ba29f49752e7e770176688796100887
Zhori <aldur92@hotmail.com>
2015-02-18 13:53:25 +0100

    Fixed spelling errors and an invalid parameter for getMessage

--- src/green_elevator/controller/MessageBuffer.java
+++ src/green_elevator/controller/MessageBuffer.java
@@ -7,32 +7,30 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class MessageBuffer {
 
-    private BlockingQueue<Message> buffer;
+	private BlockingQueue<Message> buffer;
 
-    public MessageBuffer() {
-	this.buffer = new LinkedBlockingQueue<Message>();
-    }
+	public MessageBuffer() {
+		this.buffer = new LinkedBlockingQueue<Message>();
+	}
 
-    /**
-     * Puts the message into the buffer, if no space is available until it is
-     * space available in the buffer
-     * 
-     * @param message
-     * @throws InterruptedException
-     */
-    public void putMessage(Message message) throws InterruptedException {
-	buffer.put(message);
-    }
+	/**
+	 * Puts the message into the buffer, if no space is available blocks until there is space available in the buffer.
+	 * 
+	 * @param message
+	 *            the message to add to the buffer
+	 * @throws InterruptedException
+	 */
+	public void putMessage(Message message) throws InterruptedException {
+		buffer.put(message);
+	}
 
-    /**
-     * Return the first element in the queue. Blocks until a message is
-     * available.
-     * 
-     * @param message
-     * @return
-     * @throws InterruptedException
-     */
-    public Message getMessage(Message message) throws InterruptedException {
-	return buffer.take();
-    }
+	/**
+	 * Returns the first element in the queue. Blocks until a message is available.
+	 * 
+	 * @return the first message in the queue
+	 * @throws InterruptedException
+	 */
+	public Message getMessage() throws InterruptedException {
+		return buffer.take();
+	}
 }
