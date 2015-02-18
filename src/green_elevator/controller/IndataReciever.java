dcfad4915da5f158fc714f20e94338adadb09868
ragnhild karlsson <ragnhild.karlsson@gmail.com>
2015-02-18 15:17:43 +0100

    indata-reciever

--- src/green_elevator/controller/IndataReciever.java
+++ src/green_elevator/controller/IndataReciever.java
@@ -1,15 +1,21 @@
 package green_elevator.controller;
 
+import green_elevator.controller.message.Message;
+
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Optional;
 
 public class IndataReciever implements Runnable {
 
     private final BufferedReader reader;
     private final MessageBuffer outBuffer;
+    private final MesssageTranslator messsageTranslator;
 
-    public IndataReciever(BufferedReader reader, MessageBuffer outBuffer) {
+    public IndataReciever(BufferedReader reader, MessageBuffer outBuffer, MesssageTranslator messsageTranslator) {
 	this.reader = reader;
 	this.outBuffer = outBuffer;
+	this.messsageTranslator = messsageTranslator;
     }
 
     /**
@@ -18,7 +24,17 @@ public class IndataReciever implements Runnable {
      */
     @Override
     public void run() {
-
+	String input = "";
+	try {
+	    while ((input = reader.readLine()) != null) {
+		Optional<Message> message = messsageTranslator.getMessage(input);
+		if (message.isPresent()) {
+		    outBuffer.putMessage(message.get());
+		}
+	    }
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
     }
-
 }
--- src/green_elevator/controller/command/Command.java
+++
@@ -1,9 +0,0 @@
-package green_elevator.controller.command;
-
-public interface Command {
-
-    public enum CommandType {
-	MOVECOMMAND, STOPCOMMAND, DOOROPENCOMMAND, DOORCLOSECOMMAND
-    }
-
-}
