d4d233308810e849867207fa6fdbb451adbafbc1
Zhori <aldur92@hotmail.com>
2015-03-01 18:42:33 +0100

    Fixed warnings, added get num of tasks to elevator

--- src/green_elevator/controller/Evaluator.java
+++ src/green_elevator/controller/Evaluator.java
@@ -9,75 +9,114 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Evaluator implements Runnable {
-    private ConcurrentHashMap<Integer, Elevator> elevators;
-    private MessageBuffer incommingTasks;
+	private ConcurrentHashMap<Integer, Elevator> elevators;
+	private MessageBuffer incommingTasks;
 
-    public Evaluator(ConcurrentHashMap<Integer, Elevator> elevators, MessageBuffer incommingTasks) {
-	this.elevators = elevators;
-	this.incommingTasks = incommingTasks;
-    }
-
-    @Override
-    public void run() {
-	while (true) {
-	    // getMessage is a blocking call so the thread will wait for the
-	    // buffer to be none empty.
-	    OutsideMessage message = (OutsideMessage) incommingTasks.getMessage();
-	    elevators.get(1).addTask(message);
+	public Evaluator(ConcurrentHashMap<Integer, Elevator> elevators, MessageBuffer incommingTasks) {
+		this.elevators = elevators;
+		this.incommingTasks = incommingTasks;
 	}
 
-    }
-
-    private int findOptimalElevator(OutsideMessage message) {
-	int[] goalfloors = new int[elevators.size()];
-	double[] currentPositions = new double[elevators.size()];
-	Direction[] currentOrderedDirections = new Direction[elevators.size()];
-	for (Elevator elevator : elevators.values()) {
-	    int arrayIndex = elevator.getId() - 1;
-	    goalfloors[arrayIndex] = elevator.readGoalFloor();
-	    currentPositions[arrayIndex] = elevator.readPosition();
-	    currentOrderedDirections[arrayIndex] = elevator.readDirection();
-	}
-	// sort elevators in three cetegorize
-	Set<Integer> elevatorsInRightDirection = new HashSet<Integer>();
-	Set<Integer> staticElevators = new HashSet<Integer>();
-	Set<Integer> elevatorsInWrongDirection = new HashSet<Integer>();
-	int wantedFloor = message.getFloorNumber();
-
-	for (Elevator elevator : elevators.values()) {
-	    int id = elevator.getId();
-	    Direction currentOrderedDirection = currentOrderedDirections[id - 1];
-	    if (currentOrderedDirection == Direction.STATIC) {
-		staticElevators.add(id);
-	    } else if (currentOrderedDirection == message.getDirection()) {
-		if (isInRightDirection(currentPositions[id - 1], wantedFloor, currentOrderedDirection))
-		    elevatorsInRightDirection.add(id);
-		else
-		    elevatorsInWrongDirection.add(id);
-	    } else {
-		elevatorsInWrongDirection.add(id);
-	    }
+	@Override
+	public void run() {
+		while (true) {
+			// getMessage is a blocking call so the thread will wait for the
+			// buffer to be none empty.
+			OutsideMessage message = (OutsideMessage) incommingTasks.getMessage();
+			int bestElevatorId = findOptimalElevator(message);
+			elevators.get(bestElevatorId).addTask(message);
+		}
 
 	}
 
-	return 0;
-    }
+	private int findOptimalElevator(OutsideMessage message) {
+		int[] goalfloors = new int[elevators.size()];
+		double[] currentPositions = new double[elevators.size()];
+		Direction[] currentOrderedDirections = new Direction[elevators.size()];
+		for (Elevator elevator : elevators.values()) {
+			int arrayIndex = elevator.getId() - 1;
+			goalfloors[arrayIndex] = elevator.readGoalFloor();
+			currentPositions[arrayIndex] = elevator.readPosition();
+			currentOrderedDirections[arrayIndex] = elevator.readDirection();
+		}
+		// sort elevators in three categories
+		Set<Integer> elevatorsInRightDirection = new HashSet<Integer>();
+		Set<Integer> staticElevators = new HashSet<Integer>();
+		Set<Integer> elevatorsInWrongDirection = new HashSet<Integer>();
+		int wantedFloor = message.getFloorNumber();
 
-    private boolean isInRightDirection(double currentPosition, int wantedFloor, Direction currentDirection) {
-	if (currentDirection == Direction.UP) {
-	    int nextFloor = (int) Math.ceil(currentPosition);
-	    if (nextFloor <= wantedFloor)
-		return true;
-	    else
-		return false;
+		for (Elevator elevator : elevators.values()) {
+			int id = elevator.getId();
+			Direction currentOrderedDirection = currentOrderedDirections[id - 1];
+			if (currentOrderedDirection == Direction.STATIC) {
+				staticElevators.add(id);
+			} else if (currentOrderedDirection == message.getDirection()) {
+				if (isInRightDirection(currentPositions[id - 1], wantedFloor, currentOrderedDirection,
+						goalfloors[id - 1]))
+					elevatorsInRightDirection.add(id);
+				else
+					elevatorsInWrongDirection.add(id);
+			} else {
+				elevatorsInWrongDirection.add(id);
+			}
+		}
+		// Try to first take an elevator from the group which is in the right direction with the shortest path to the
+		// wanted floor
+		if (!elevatorsInRightDirection.isEmpty())
+			return elevatorWithShortestPath(elevatorsInRightDirection, currentPositions, wantedFloor);
+
+		// Else we try to get a static elevator with the shortest path to the wanted floor
+		if (!staticElevators.isEmpty()) {
+			return elevatorWithShortestPath(staticElevators, currentPositions, wantedFloor);
+		}
+		// If we have no other option we take the elevator with the current least tasks
+		return elevatorWithLeastTasks(elevatorsInWrongDirection);
 	}
-	if (currentDirection == Direction.DOWN) {
-	    int nextFloor = (int) Math.floor(currentPosition);
-	    if (nextFloor >= wantedFloor)
-		return true;
-	    else
-		return false;
+
+	private int elevatorWithLeastTasks(Set<Integer> elevatorsInWrongDirection) {
+		int minTaskAmount = Integer.MAX_VALUE;
+		int bestElevatorId = 0;
+		for (int id : elevatorsInWrongDirection) {
+			int nTasks = elevators.get(id).numberOfTasks();
+			if (nTasks < minTaskAmount) {
+				minTaskAmount = nTasks;
+				bestElevatorId = id;
+			}
+		}
+		return bestElevatorId;
 	}
-	throw new IllegalStateException();
-    }
+
+	private int elevatorWithShortestPath(Set<Integer> elevators, double[] currentPositions, int wantedFloor) {
+
+		double shortestDistance = Double.MAX_VALUE;
+		int bestElevatorId = 0;
+		for (int id : elevators) {
+			double distance = Math.abs(currentPositions[id - 1] - wantedFloor);
+			if (distance < shortestDistance) {
+				shortestDistance = distance;
+				bestElevatorId = id;
+			}
+
+		}
+		return bestElevatorId;
+	}
+
+	private boolean isInRightDirection(double currentPosition, int wantedFloor, Direction currentDirection,
+			int currentGoalFloor) {
+		if (currentDirection == Direction.UP) {
+			int nextFloor = (int) Math.ceil(currentPosition);
+			if (nextFloor <= wantedFloor || currentGoalFloor <= wantedFloor)
+				return true;
+			else
+				return false;
+		}
+		if (currentDirection == Direction.DOWN) {
+			int nextFloor = (int) Math.floor(currentPosition);
+			if (nextFloor >= wantedFloor || currentGoalFloor >= wantedFloor)
+				return true;
+			else
+				return false;
+		}
+		throw new IllegalStateException();
+	}
 }
--- src/green_elevator/controller/elevator/TaskManager.java
+++ src/green_elevator/controller/elevator/TaskManager.java
@@ -15,148 +15,146 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class TaskManager {
 
-    private BlockingQueue<Task> tasks;
-    private int taskId;
-    private final boolean debug = true;
+	private BlockingQueue<Task> tasks;
+	private int taskId;
+	private final boolean debug = true;
 
-    public TaskManager() {
+	public TaskManager() {
 
-	this.tasks = new LinkedBlockingQueue<Task>();
-	this.taskId = 0;
-    }
-
-    public boolean addTask(Message command, double currentPosition, int goalFloor, Direction direction) {
-
-	if (command.getMessageType() == MessageType.INSIDEMESSAGE) {
-	    InsideMessage insideMessage = (InsideMessage) command;
-	    int wantedFloor = insideMessage.getFloorNumber();
-	    try {
-		tasks.put(new Task(getTaskID(), TaskType.INSIDETASK, Optional.empty(), wantedFloor));
-
-	    } catch (InterruptedException e) {
-		e.printStackTrace();
-	    }
+		this.tasks = new LinkedBlockingQueue<Task>();
+		this.taskId = 0;
 	}
 
-	if (command.getMessageType() == MessageType.OUTSIDEMESSAGE) {
-	    OutsideMessage outsideMessage = (OutsideMessage) command;
-	    int wantedFloor = outsideMessage.getFloorNumber();
-	    tasks.add(new Task(getTaskID(), TaskType.OUTSIDETASK, Optional.of(outsideMessage.getDirection()),
-		    wantedFloor));
+	public boolean addTask(Message command, double currentPosition, int goalFloor, Direction direction) {
+
+		if (command.getMessageType() == MessageType.INSIDEMESSAGE) {
+			InsideMessage insideMessage = (InsideMessage) command;
+			int wantedFloor = insideMessage.getFloorNumber();
+			try {
+				tasks.put(new Task(getTaskID(), TaskType.INSIDETASK, Optional.empty(), wantedFloor));
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (command.getMessageType() == MessageType.OUTSIDEMESSAGE) {
+			OutsideMessage outsideMessage = (OutsideMessage) command;
+			int wantedFloor = outsideMessage.getFloorNumber();
+			tasks.add(new Task(getTaskID(), TaskType.OUTSIDETASK, Optional.of(outsideMessage.getDirection()),
+					wantedFloor));
+		}
+		return true;
 	}
-	return true;
-    }
 
-    private int getTaskID() {
-	taskId++;
-	return taskId;
-    }
-
-    public int getNumberOfTasks() {
-	int size = tasks.size();
-	return size;
-
-    }
-
-    /**
-     * Returns a task if any is available Inside task are prioritized first
-     * Outside tasks are prioritized second in distance order.
-     * 
-     * @return
-     */
-    public Optional<Task> tryGetTask(double position, Direction direction) {
-	int presentFloor = (int) Math.round(position);
-	// try to first find inside commands in right direction
-	for (Task task : tasks) {
-	    if ((task.getTaskType() == TaskType.INSIDETASK)) {
-		if (direction == direction.UP && task.getGoalFloor() > presentFloor)
-		    return Optional.of(task);
-		if ((direction == direction.DOWN && task.getGoalFloor() < presentFloor))
-		    return Optional.of(task);
-	    }
+	private int getTaskID() {
+		taskId++;
+		return taskId;
 	}
-	// Take any inside task
-	for (Task task : tasks) {
-	    if ((task.getTaskType() == TaskType.INSIDETASK))
-		return Optional.of(task);
+
+	public int getNumberOfTasks() {
+		int size = tasks.size();
+		return size;
+
 	}
-	// Get the outside task that is closest to present position
-	Task bestTask = null;
-	int bestDiff = Integer.MAX_VALUE;
-	for (Task task : tasks) {
-	    if ((Math.abs(task.getGoalFloor() - presentFloor)) < bestDiff) {
-		bestDiff = Math.abs(task.getGoalFloor() - presentFloor);
-		bestTask = task;
-	    }
+
+	/**
+	 * Returns a task if any is available Inside task are prioritized first Outside tasks are prioritized second in
+	 * distance order.
+	 * 
+	 * @return
+	 */
+	public Optional<Task> tryGetTask(double position, Direction direction) {
+		int presentFloor = (int) Math.round(position);
+		// try to first find inside commands in right direction
+		for (Task task : tasks) {
+			if ((task.getTaskType() == TaskType.INSIDETASK)) {
+				if (direction == Direction.UP && task.getGoalFloor() > presentFloor)
+					return Optional.of(task);
+				if ((direction == Direction.DOWN && task.getGoalFloor() < presentFloor))
+					return Optional.of(task);
+			}
+		}
+		// Take any inside task
+		for (Task task : tasks) {
+			if ((task.getTaskType() == TaskType.INSIDETASK))
+				return Optional.of(task);
+		}
+		// Get the outside task that is closest to present position
+		Task bestTask = null;
+		int bestDiff = Integer.MAX_VALUE;
+		for (Task task : tasks) {
+			if ((Math.abs(task.getGoalFloor() - presentFloor)) < bestDiff) {
+				bestDiff = Math.abs(task.getGoalFloor() - presentFloor);
+				bestTask = task;
+			}
+		}
+		if (bestTask != null)
+			return Optional.of(bestTask);
+		else
+			return Optional.empty();
 	}
-	if (bestTask != null)
-	    return Optional.of(bestTask);
-	else
-	    return Optional.empty();
-    }
 
-    /**
-     * A blocking call that returns any available task;
-     * 
-     * @return
-     */
-    public Task waitForAnyNewTask() {
-	Task task;
-	try {
-	    task = tasks.take();
-	    tasks.put(task);
-	    return task;
-	} catch (InterruptedException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+	/**
+	 * A blocking call that returns any available task;
+	 * 
+	 * @return
+	 */
+	public Task waitForAnyNewTask() {
+		Task task;
+		try {
+			task = tasks.take();
+			tasks.put(task);
+			return task;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		throw new IllegalStateException();
 	}
-	throw new IllegalStateException();
-    }
 
-    /**
-     * Evaluates if the elevator should stop at next floor given the position,
-     * direction and goalFloor of the elevator
-     * 
-     * @param position
-     *            of the asking elevator
-     * @param direction
-     *            of the asking elevator
-     * @param goalFloor
-     *            of the asking elevator
-     * @return
-     */
-    public boolean shouldStop(double position, Direction direction, int goalFloor) {
+	/**
+	 * Evaluates if the elevator should stop at next floor given the position, direction and goalFloor of the elevator
+	 * 
+	 * @param position
+	 *            of the asking elevator
+	 * @param direction
+	 *            of the asking elevator
+	 * @param goalFloor
+	 *            of the asking elevator
+	 * @return
+	 */
+	public boolean shouldStop(double position, Direction direction, int goalFloor) {
 
-	boolean shouldStop = false;
-	int closestFloor = (int) Math.round(position);
-	if ((direction == Direction.UP) && (goalFloor < closestFloor))
-	    return false;
+		boolean shouldStop = false;
+		int closestFloor = (int) Math.round(position);
+		if ((direction == Direction.UP) && (goalFloor < closestFloor))
+			return false;
 
-	if ((direction == Direction.DOWN) && (goalFloor > closestFloor))
-	    return false;
+		if ((direction == Direction.DOWN) && (goalFloor > closestFloor))
+			return false;
 
-	Iterator<Task> iterator = tasks.iterator();
-	while (iterator.hasNext()) {
+		Iterator<Task> iterator = tasks.iterator();
+		while (iterator.hasNext()) {
 
-	    Task task = iterator.next();
-	    // remove all inner tasks which wants to stop at this floor
-	    if ((task.getTaskType() == TaskType.INSIDETASK) && (task.getGoalFloor() == closestFloor)) {
-		iterator.remove();
-		shouldStop = true;
-	    }
-	    // remove outside tasks which wants to travel in the same direction
-	    // and who is waiting at the closing floor
-	    // check if the true direction and the given direction is in
-	    // conflict and goal floor != closest floor
+			Task task = iterator.next();
+			// remove all inner tasks which wants to stop at this floor
+			if ((task.getTaskType() == TaskType.INSIDETASK) && (task.getGoalFloor() == closestFloor)) {
+				iterator.remove();
+				shouldStop = true;
+			}
+			// remove outside tasks which wants to travel in the same direction
+			// and who is waiting at the closing floor
+			// check if the true direction and the given direction is in
+			// conflict and goal floor != closest floor
 
-	    if ((task.getTaskType() == TaskType.OUTSIDETASK) && (task.getDirection().get() == direction)
-		    && (task.getGoalFloor() == closestFloor)) {
-		if (debug)
-		    System.out.println("Taskmanager removed " + task.toString());
-		iterator.remove();
-		shouldStop = true;
-	    }
+			if ((task.getTaskType() == TaskType.OUTSIDETASK) && (task.getDirection().get() == direction)
+					&& (task.getGoalFloor() == closestFloor)) {
+				if (debug)
+					System.out.println("Taskmanager removed " + task.toString());
+				iterator.remove();
+				shouldStop = true;
+			}
+		}
+		return shouldStop;
 	}
-	return shouldStop;
-    }
 }
