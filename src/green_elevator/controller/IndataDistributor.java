d80ff9287e4e9da35eda37ba54559372edcf87b0
Zhori <aldur92@hotmail.com>
2015-03-01 18:41:15 +0100

    Fixed insideMessage handling in IndataDistributor

--- src/green_elevator/controller/IndataDistributor.java
+++ src/green_elevator/controller/IndataDistributor.java
@@ -1,6 +1,7 @@
 package green_elevator.controller;
 
 import green_elevator.controller.elevator.Elevator;
+import green_elevator.controller.message.InsideMessage;
 import green_elevator.controller.message.Message;
 import green_elevator.controller.message.PositionMessage;
 import green_elevator.controller.message.StopMessage;
@@ -9,41 +10,47 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class IndataDistributor implements Runnable {
 
-    private final MessageBuffer incommingData;
-    private final MessageBuffer commandBuffer;
-    private final ConcurrentHashMap<Integer, Elevator> elevators;
+	private final MessageBuffer incommingData;
+	private final MessageBuffer commandBuffer;
+	private final ConcurrentHashMap<Integer, Elevator> elevators;
 
-    public IndataDistributor(MessageBuffer incommingData, MessageBuffer commandBuffer,
-	    ConcurrentHashMap<Integer, Elevator> elevators) {
-	this.incommingData = incommingData;
-	this.commandBuffer = commandBuffer;
-	this.elevators = elevators;
+	public IndataDistributor(MessageBuffer incommingData, MessageBuffer commandBuffer,
+			ConcurrentHashMap<Integer, Elevator> elevators) {
+		this.incommingData = incommingData;
+		this.commandBuffer = commandBuffer;
+		this.elevators = elevators;
 
-    }
-
-    @Override
-    public void run() {
-	Elevator elevator;
-	int elevatorId;
-	while (true) {
-	    Message message = incommingData.getMessage();
-	    switch (message.getMessageType()) {
-
-	    case POSITIONMESSAGE:
-		PositionMessage positionMessage = (PositionMessage) message;
-		elevatorId = positionMessage.getElevatorNumber();
-		elevator = elevators.get(elevatorId);
-		elevator.addPositionData(positionMessage.getPosition());
-		break;
-	    case STOPMESSAGE:
-		StopMessage stopMessage = (StopMessage) message;
-		elevatorId = stopMessage.getElevatorNumber();
-		elevator = elevators.get(elevatorId);
-		elevator.addTask(stopMessage);
-		break;
-	    default:
-		commandBuffer.putMessage(message);
-	    }
 	}
-    }
+
+	@Override
+	public void run() {
+		Elevator elevator;
+		int elevatorId;
+		while (true) {
+			Message message = incommingData.getMessage();
+			switch (message.getMessageType()) {
+
+			case POSITIONMESSAGE:
+				PositionMessage positionMessage = (PositionMessage) message;
+				elevatorId = positionMessage.getElevatorNumber();
+				elevator = elevators.get(elevatorId);
+				elevator.addPositionData(positionMessage.getPosition());
+				break;
+			case STOPMESSAGE:
+				StopMessage stopMessage = (StopMessage) message;
+				elevatorId = stopMessage.getElevatorNumber();
+				elevator = elevators.get(elevatorId);
+				elevator.addTask(stopMessage);
+				break;
+			case INSIDEMESSAGE:
+				InsideMessage insideMessage = (InsideMessage) message;
+				elevatorId = insideMessage.getElevatorNumber();
+				elevator = elevators.get(elevatorId);
+				elevator.addTask(insideMessage);
+				break;
+			default:
+				commandBuffer.putMessage(message);
+			}
+		}
+	}
 }
